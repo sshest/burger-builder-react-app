437ae7df07c12f0f3483e27b6ed6447fbb5ce644
Sergey.Shestakov <sergey.shestakov@dataart.com>
2019-07-01 18:25:51 +0300

    Add initial boilerplate

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Auxilary';
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Burger controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
