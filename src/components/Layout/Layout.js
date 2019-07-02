d19ebaace80cb61b4b35cd84bee970b6b1a9bb32
Sergii.Shestakov <sshest78@gmail.com>
2019-07-03 00:33:10 +0300

    Toggle SideDrawer with click

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -16,10 +16,18 @@ class Layout extends Component {
         })
     };
 
+    sideDrawerToggleClickHandler = () => {
+        this.setState((prevState) => {
+            return {
+                showSideDrawer: !prevState.showSideDrawer
+            }
+        });
+    };
+
     render() {
         return (
             <Aux>
-                <Toolbar />
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleClickHandler}/>
                 <SideDrawer open={this.state.showSideDrawer}
                             closed={this.sideDrawerCloseHandler}/>
                 <main className={classes.Content}>
