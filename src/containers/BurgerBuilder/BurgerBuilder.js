cf243f80ebe2eef6d8985a6ee92dc95d28cda98e
Sergey.Shestakov <sergey.shestakov@dataart.com>
2019-07-02 16:04:07 +0300

    Display burger price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,10 +5,10 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 const INGREDIENT_PRICES = {
-    salad: 0.5,
-    cheese: 0.4,
-    meat: 1.3,
-    bacon: 0.7
+    Salad: 0.5,
+    Cheese: 0.4,
+    Meat: 1.3,
+    Bacon: 0.7
 };
 
 class BurgerBuilder extends Component {
@@ -63,6 +63,7 @@ class BurgerBuilder extends Component {
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
+                    price={this.state.totalPrice}
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
