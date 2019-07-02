d7e15d554352106ec9275fbe0496629cb7f8b265
Sergii.Shestakov <sshest78@gmail.com>
2019-07-02 22:54:17 +0300

    Add Navigation components

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import Aux from '../../hoc/Auxilary';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 import classes from './Layout.css';
 
 const layout = (props) => (
     <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar></Toolbar>
         <main className={classes.Content}>
             {props.children}
         </main>
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo';
+
+const toolbar = (props) => (
+    <header className={classes.Toolbar}>
+        <div>Menu</div>
+        <Logo/>
+        <nav>
+            <ul>
+
+            </ul>
+        </nav>
+    </header>
+);
+
+export default toolbar;
