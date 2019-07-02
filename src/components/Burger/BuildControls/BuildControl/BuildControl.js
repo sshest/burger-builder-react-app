c705bf374e61ed4f6828f1be32f66e5247bedabe
Sergey.Shestakov <sergey.shestakov@dataart.com>
2019-07-02 15:55:06 +0300

    Disable Less button on ingredient absence

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -7,7 +7,8 @@ const buildControl = (props) => (
         <div className={classes.Label}>{props.label}</div>
         <button
             className={classes.Less}
-            onClick={props.removed}>Less</button>
+            onClick={props.removed}
+            disabled={props.disabled}>Less</button>
         <button
             className={classes.More}
             onClick={props.added}>More</button>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -36,7 +36,11 @@ class BurgerBuilder extends Component {
     }
 
     removeIngredientHandler = (type) => {
-        const updatedCount = this.state.ingredients[type] - 1;
+        const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0) {
+            return;
+        }
+        const updatedCount =  oldCount - 1;
         const updatedIngredients = {
             ...this.state.ingredients
         };
@@ -49,12 +53,19 @@ class BurgerBuilder extends Component {
     }
 
     render() {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo}
                 />
             </Aux>
         );
