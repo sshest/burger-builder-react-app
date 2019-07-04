b701fa1d664e36ad9ca816d49e24a2a4867a7f78
Sergey.Shestakov <sergey.shestakov@dataart.com>
2019-07-04 16:55:48 +0300

    Add order reducer

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,2 +1,27 @@
 import * as actionTypes from '../actions/actionTypes';
 
+const initialState = {
+    orders: [],
+    // purchasing: false,
+    loading: false
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.PURCHASE_BURGER_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                orders: state.orders.concat({...action.orderData, id: action.orderID})
+            };
+        case actionTypes.PURCHASE_BURGER_FAIL:
+            return {
+                ...state,
+                loading: false,
+            };
+        default:
+            return state;
+    }
+};
+
+export default reducer;
