b0dd8972a12ad06d6da382084ddbca5e47ece349
Sergey.Shestakov <sergey.shestakov@dataart.com>
2019-07-05 11:09:59 +0300

    Add lazy loading for /checkout and /orders routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import asyncComponent from './hoc/asyncComponent';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Checkout/Orders/Orders'
+
+const asyncCheckout = asyncComponent(() => {
+    return import('./containers/Checkout/Checkout');
+});
+
+const asyncOrders = asyncComponent(() => {
+    return import('./containers/Checkout/Orders/Orders');
+});
 
 function App() {
   return (
     <div className="App">
       <Layout>
           <Switch>
-              <Route path="/checkout" component={Checkout} />
-              <Route path="/orders" component={Orders} />
+              <Route path="/checkout" component={asyncCheckout} />
+              <Route path="/orders" component={asyncOrders} />
               <Route path="/" component={BurgerBuilder}/>
           </Switch>
       </Layout>
